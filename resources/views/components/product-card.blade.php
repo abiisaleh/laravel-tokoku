fec96131885a831ccfa887ff37a854c56e8e15bd
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-06-30 18:47:00 +0900

    Update product-card.blade.php

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,6 +1,6 @@
 <a href="/product/{{ $product->id }}" class="group">
     <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-        <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
+        <img src="{{$product->gambar}}"
             alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
             class="h-full w-full object-cover object-center group-hover:opacity-75">
     </div>
