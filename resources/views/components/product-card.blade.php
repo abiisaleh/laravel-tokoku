05bb7352aefa1510818b6d43eb167d4fd05a003a
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-06-30 18:51:43 +0900

    Update product-card.blade.php

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,6 +1,6 @@
 <a href="/product/{{ $product->id }}" class="group">
     <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-        <img src="{{$product->gambar}}"
+        <img src="/storage/{{$product->gambar}}"
             alt="{{$product->gambar}}"
             class="h-full w-full object-cover object-center group-hover:opacity-75">
     </div>
