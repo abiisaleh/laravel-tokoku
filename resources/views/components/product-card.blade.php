40c7727bf9e325fc94590fc364d82d297b4a2ef4
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-06-30 18:49:34 +0900

    Update product-card.blade.php

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,7 +1,7 @@
 <a href="/product/{{ $product->id }}" class="group">
     <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img src="{{$product->gambar}}"
-            alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+            alt="{{$product->gambar}}"
             class="h-full w-full object-cover object-center group-hover:opacity-75">
     </div>
     <h3 class="mt-4 text-sm text-gray-700">{{ $product->nama }}</h3>
