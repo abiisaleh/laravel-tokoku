657472bb9ee1b631c9f9f163644fad00a27461ea
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-07-04 07:47:12 +0900

    Update ProductResource.php

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -19,6 +19,10 @@ class ProductResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-cube';
 
+    protected static ?string $navigationGroup = 'Shop';
+    
+    protected static ?int $navigationSort = 1;
+
     public static function form(Form $form): Form
     {
         return $form
